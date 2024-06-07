038ba78fca2af8bed251232ef55f3dad29d3ac97
Michael Henderson <mdhender@mdhender.com>
2024-06-07 16:32:51 -0600

    fixed nav bar for hero pages

--- app/components/hero-shell-nav-bar.js
+++ app/components/hero-shell-nav-bar.js
@@ -3,6 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class HeroShellNavBarComponent extends Component {
+  isIndexPage = this.args.page === 'index';
+  isFeaturesPage = this.args.page === 'features';
+
   @tracked isHamburgerMenuOpen = false;
 
   @action
--- app/components/hero-shell.js
+++ app/components/hero-shell.js
@@ -2,8 +2,4 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
 export default class HeroShellComponent extends Component {
-  @service router;
-
-  isIndexPage = this.router.currentRouteName === 'index';
-  isFeaturesPage = this.router.currentRouteName === 'features';
 }
