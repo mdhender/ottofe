9d3c87aae6dcbdd8581940f0abdf0bbcb4f6aea5
Michael Henderson <mdhender@mdhender.com>
2024-06-06 20:16:13 -0600

    created AppShell component for reports

--- app/components/app-shell.js
+++ app/components/app-shell.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class FeaturesComponent extends Component {
+export default class ShellComponent extends Component {
   @tracked isMobileMenuOpen = false;
 
   @action
--- tests/integration/components/app-shell-test.js
+++ tests/integration/components/app-shell-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ottofe/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | features', function (hooks) {
+module('Integration | Component | app-shell', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Features />`);
+    await render(hbs`<AppShell />`);
 
     assert.dom().hasText('');
 
     // Template block usage:
     await render(hbs`
-      <Features>
+      <AppShell>
         template block text
-      </Features>
+      </AppShell>
     `);
 
     assert.dom().hasText('template block text');
