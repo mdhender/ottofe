22e7bc224bd4d57a69da3ed697e3655e7d64fc92
Michael Henderson <mdhender@mdhender.com>
2024-06-07 09:59:21 -0600

    started reports page in app shell

--- app/components/app-shell.js
+++ app/components/app-shell.js
@@ -1,12 +1,23 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class ShellComponent extends Component {
+export default class AppShellComponent extends Component {
+  @service router;
+
   @tracked isMobileMenuOpen = false;
+  @tracked isProfileMenuOpen = false;
+
+  activeRoute = this.router.currentRouteName;
 
   @action
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  @action
+  toggleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
   }
 }
